d66e73124efd74cde53d164af810bf4ad4c155ef
nightdiverru <xwebmaste@gmail.com>
2019-12-20 16:30:07 +0300

    no message

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
+sudo apt install -y git
 git clone -b monolith https://github.com/express42/reddit.git
 cd reddit && bundle install
-sudo systemctl start mongod
-sudo systemctl enable mongod
 puma -d
