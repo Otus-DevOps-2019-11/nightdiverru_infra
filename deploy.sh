1263e23d32e5b3fb171b34d0ccf076cba7cd08bc
nightdiverru <xwebmaste@gmail.com>
2019-12-23 10:33:36 +0300

    no message

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+#
 sudo apt install -y git
 git clone -b monolith https://github.com/express42/reddit.git
 cd reddit && bundle install
--- install_mongodb.sh
+++ install_mongodb.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+#
 wget -qO - https://www.mongodb.org/static/pgp/server-3.2.asc | sudo apt-key add -
 sudo bash -c 'echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" > /etc/apt/sources.list.d/mongodb-org-3.2.list'
 sudo apt update
